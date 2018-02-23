821c26edd1036ef3fcab890b3ca101f56f2fc723
Christine Ibaraki <>
2018-02-23 01:13:49 -0500

    add

--- index.js
+++ index.js
@@ -5,14 +5,8 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hi')
+  response.sendFile('index.html');
 })
-
-app.get('/',function(req,res){
-       
-     res.sendFile('index.html');
-
-});
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
